af2fe92f29161a8c6c64261aa465922b5aed6d6b
Carlo331 <davidcarlanton06@gmail.com>
2024-01-05 15:16:55 +0100

    blackjack layout

--- src/routes/blackjack/+page.svelte
+++ src/routes/blackjack/+page.svelte
@@ -1,20 +1,20 @@
 <script>
 	import Chat from '../chat.svelte'
 	import Header from '../header.svelte'
-
 </script>
 
-<div class="flex flex-col h-screen w-screen">
-    <div class="w-4/5 h-1/6 bg-blue-700">
-        <Chat/>
-    </div>
-    <div class="flex items-center justify-between bg-primary-700 h-5/6 w-4/5">
-        <div class="flex  h-5/6 w-1/5">
+<div class="flex justify-between h-screen w-screen">
+    <div class="flex flex-col h-screen w-4/5">
+        <Header/>
+        <div class="flex items-center justify-between bg-primary-700 h-5/6 w-full">
+            <div class="flex  h-full w-1/3">
 
-        </div>
-        <div class="flex w-3/5 h-9/12 bg-primary-900">
-
+            </div>
+            <div class="flex bg-primary-900 h-[92%] w-[58%] mr-[2.1%]">
+                
+            </div>
         </div>
     </div>
+     <Chat/>
 </div>
     
