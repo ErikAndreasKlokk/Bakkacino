25f5620c872b8c5ee54b0bbc09117ceac6d01079
ErikAndreasKlokk <eaklokk@gmail.com>
2024-01-05 13:11:08 +0100

    added chat component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<div class=" justify-center items-center">hei</div>
+<script>
+	import Chat from './chat.svelte'
+
+</script>
+
+<main class=" w-screen h-screen flex justify-between">
+	<div class=" w-40 h-40 flex justify-center items-center">
+		<p>hei</p>
+	</div>
+	<Chat />
+</main> 
---
+++ src/routes/chat.svelte
@@ -0,0 +1,5 @@
+<main class="flex h-screen w-1/5 border-solid border-4 border-x-emerald-500">
+    <div></div>
+    <div></div>
+    <div></div>
+</main>
