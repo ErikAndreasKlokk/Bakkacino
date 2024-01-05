da68bf3771abbab7c8dc40c60d6f8d2ad8e49e84
ErikAndreasKlokk <eaklokk@gmail.com>
2024-01-05 13:30:53 +0100

    added firebase

---
+++ src/routes/firebase.svelte
@@ -0,0 +1,19 @@
+<script>
+    // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXRxjSYJSKh4Qro2gLpdeSAbbsNwH9uFw",
+  authDomain: "bakkacino.firebaseapp.com",
+  projectId: "bakkacino",
+  storageBucket: "bakkacino.appspot.com",
+  messagingSenderId: "816188495598",
+  appId: "1:816188495598:web:8231919b5907aa778305ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+</script>
