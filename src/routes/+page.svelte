69d964074443063388e70d15496f73c120afe8f4
ErikAndreasKlokk <eaklokk@gmail.com>
2024-01-09 11:54:20 +0100

    la til en timer på coindrop og lagde en logo til coindrop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,35 @@
 <script>
 	import Chat from './chat.svelte'
 	import Header from './header.svelte'
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+
+
+		function startTimer(duration, display) {
+			var timer = duration, minutes, seconds;
+			setInterval(function () {
+				minutes = parseInt(timer / 60, 10)
+				seconds = parseInt(timer % 60, 10);
+
+				minutes = minutes < 10 ? "0" + minutes : minutes;
+				seconds = seconds < 10 ? "0" + seconds : seconds;
+
+				display.innerHTML = minutes + ":" + seconds;
+
+				if (--timer < 0) {
+					timer = 0;
+					// timer = duration; // uncomment this line to reset timer automatically after reaching 0
+				}
+			}, 1000);
+		}
+
+
+		var time = 3600 / 2; // your time in seconds here
+		let display = document.querySelector('#safeTimerDisplay');
+		startTimer(time, display);
+	})
+
 
 </script>
 
@@ -8,12 +37,15 @@
 	<div class=" w-4/5 h-screen">
 		<Header />
 		<div class=" p-10">
-			<div class=" flex w-full max-h-1/6 justify-between items-center p-5 bg-primary-900 border-primary-500 border-2 rounded-3xl">
+			<div class=" flex w-full max-h-1/6 justify-between items-center p-5 border-primary-500 border-2 rounded-3xl bg-gradient-to-r from-transparent to-primary-900">
 				<div class=" flex w-1/2 justify-between">
-					<div class=" flex items-center">Coindrop</div>
-					<div class=" w-4/5">
-						<div>Timer</div>
-						<div>Coindrop's skjer hvert 30'ende minutt og gir alle som deltar en del av pengene i Coindrop's potten.</div>
+					<div class=" flex items-center mr-2"><img class=" w-40" src="/CoinDrops.png" alt="Coindrops logo"></div>
+					<div class=" w-4/5 flex flex-col ml-3">
+						<div class=" bg-gradient-to-r from-primary-900 to-primary-700 w-24 flex justify-center items-center text-surface-50 font-extrabold tracking-widest border-2 border-primary-600 rounded-md mb-2">
+							<img class=" mr-2 " src="/clock.svg" alt="klokke">
+							<div class=" w-4/6" id="safeTimerDisplay"></div>
+						</div>
+						<div class=" text-xs">Coindrop's skjer hvert <span class=" font-extrabold text-gray-50">30. minutt</span> og gir alle som deltar en del av pengene i <span class=" font-extrabold text-primary-500">Coindrop's potten</span>.</div>
 					</div>
 				</div>
 				<div class=" flex">
@@ -23,7 +55,7 @@
 			</div>
 			<div class=" flex flex-col mt-10">
 				<div>
-					<p class=" font-extrabold text-xl">Games</p>
+					<p class=" font-extrabold text-xl">Bakkacino Orginaler</p>
 				</div>
 				<div class=" flex mt-3" >
 					<a href="/blackjack" class=" relative h-52 w-52 bg-primary-900 cursor-pointer border-2 border-primary-500 rounded-lg overflow-hidden">
