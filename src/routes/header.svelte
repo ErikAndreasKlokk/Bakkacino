ceb83e95503562b09fe92f179720c5ab1150218d
ErikAndreasKlokk <eaklokk@gmail.com>
2024-01-10 08:54:38 +0100

    header style finished

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -17,17 +17,17 @@
 
 </script>
 
-<header class="flex justify-between w-[calc(100%-20.625rem)] h-24 items-center shadow-xl fixed top-0 left-0 z-50 bg-surface-800">
+<header class="flex justify-between w-[calc(100%-20.625rem)] h-24 items-center shadow-xl fixed top-0 left-0 z-50 bg-surface-800 px-10">
     <div class=" flex items-center justify-between">
         <a class=" h-10 w-10 flex items-center justify-center" href="/">
             <img class=" h-8 w-8" src="/home.svg" alt="home">
         </a>
-        <div class=" h-8 border-r-2 border-primary-700"></div>
-        <button class="btn variant-filled" use:popup={popupFeatured}>Show Popup</button>      
+        <div class=" h-8 border-r-2 border-primary-700 mx-5"></div>
+        <button class=" flex items-center justify-between p-1 text-surface-400 font-bold" use:popup={popupFeatured}>Games <span><img src="/expand.svg" alt="expand games"></span></button>
         <div class="card p-4 w-72 shadow-xl" data-popup="popupFeatured">
             <div><p>Demo Content</p></div>
             <div class="arrow bg-surface-100-800-token" />
-        </div>			
+        </div>
     </div>
     <div class=" flex items-center bg-surface-900 p-2 border border-primary-700 rounded-lg">
         <img class=" h-5" src="/Gold-Coin.png" alt="gold coin">
