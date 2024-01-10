5f9a831f0264c489a9fdf4d1028e89f2d35d304e
ErikAndreasKlokk <eaklokk@gmail.com>
2024-01-10 12:08:32 +0100

    oppdaterte header

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -19,20 +19,20 @@
 
 <header class="flex justify-between w-[calc(100%-20.625rem)] h-24 items-center shadow-xl fixed top-0 left-0 z-50 bg-surface-800 px-10">
     <div class=" flex items-center justify-between">
-        <a class=" h-10 w-10 flex items-center justify-center" href="/">
+        <a class=" h-12 w-12 flex items-center justify-center rounded-lg border border-transparent hover:border-primary-700/90" href="/">
             <img class=" h-8 w-8" src="/home.svg" alt="home">
         </a>
         <div class=" h-8 border-r-2 border-primary-700 mx-5"></div>
-        <button class=" flex items-center justify-between p-1 text-surface-400 font-bold" use:popup={popupFeatured}>Games <span><img src="/expand.svg" alt="expand games"></span></button>
+        <button class=" flex items-center justify-between p-2 text-surface-400 font-bold rounded-lg border border-transparent hover:border-primary-700/90" use:popup={popupFeatured}>Games <span><img src="/expand.svg" alt="expand games"></span></button>
         <div class="" data-popup="popupFeatured">
             <div class=" flex mt-3 bg-surface-800 rounded-lg" >
-                <a href="/blackjack" class=" relative h-40 w-40 bg-primary-900 cursor-pointer rounded-lg overflow-hidden mx-3">
+                <a href="/blackjack" class=" relative h-40 w-40 cursor-pointer rounded-lg overflow-hidden m-3 border border-transparent hover:border-primary-700/90">
                     <img src="/blackjack.png" alt="blackjack">
                     <div class=" flex justify-center items-center absolute bottom-0 w-full h-1/4 bg-surface-800 z-10">
                         <p class=" font-extrabold tracking-wider text-lg">Blackjack</p>
                     </div>
                 </a>
-                <a href="/" class=" relative h-40 w-40 bg-primary-900 cursor-pointer rounded-lg overflow-hidden mx-3">
+                <a href="/" class=" relative h-40 w-40 cursor-pointer rounded-lg overflow-hidden m-3 border border-transparent hover:border-primary-700/90">
                     <img src="/blackjack.png" alt="blackjack">
                     <div class=" flex justify-center items-center absolute bottom-0 w-full h-1/4 bg-surface-800 z-10">
                         <p class=" font-extrabold tracking-wider text-lg">Blackjack</p>
@@ -46,5 +46,5 @@
         <p class=" mr-3 ml-2 font-black font-family-bakka text-sm">0.00</p>
         <span class="flex h-5.5 mr-1 items-center rounded-lg border border-surface-600 bg-surface-800 px-1.5 pt-0.25 text-[0.625rem] font-bold text-gray-400">COINS</span>
     </div>
-    <Avatar initials="EK" background="bg-surface-600" />
+    <Avatar class="border border-transparent hover:border-primary-700/90" initials="EK" background="bg-surface-600" />
 </header>
