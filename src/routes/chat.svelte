c85907d5ecd35ffc2c84a2f6835d8a5286258e7f
ErikAndreasKlokk <eaklokk@gmail.com>
2024-01-10 09:28:30 +0100

    endret på logo og coindrops bildet og startet på chatten

--- src/routes/chat.svelte
+++ src/routes/chat.svelte
@@ -1,5 +1,5 @@
-<main class="flex h-screen w-[20.625rem] border-solid border-4 border-emerald-500 fixed top-0 right-0 z-50 bg-surface-800">
-    <div></div>
+<main class="flex h-screen w-[20.625rem] fixed top-0 right-0 z-50 bg-surface-900 flex-col border-l-4 border-surface-800 " >
+    <div class=""><img src="/bakkacino.png" alt="logo bakkacino"></div>
     <div></div>
     <div></div>
 </main>
