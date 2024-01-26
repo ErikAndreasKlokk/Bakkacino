58a3f6bf3497dacbad2b0d07521e0f062519b60a
ErikAndreasKlokk <eaklokk@gmail.com>
2024-01-26 15:21:16 +0100

    profil modal

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -7,30 +7,51 @@
     // provided by Modals
     export let isOpen
 
-    const En_Ti = " bg-surface-600 border border-surface-400"
-    const Elve_Tjuefem = " bg-blue-600/40 border border-blue-400/70"
-    const Tjueseks_Femtifem = " bg-pink-600/60 border border-pink-400"
-    const Femtiseks_Åtti = " bg-primary-700/80 border border-primary-500"
-    const Åttien_Nittini = " bg-yellow-600/50 border border-yellow-400/70"
+    let En_Ti = " bg-surface-600 border border-surface-400"
+    let Elve_Tjuefem = " bg-blue-600/40 border border-blue-400/70"
+    let Tjueseks_Femtifem = " bg-pink-600/60 border border-pink-400"
+    let Femtiseks_Åtti = " bg-primary-700/80 border border-primary-500"
+    let Åttien_Nittini = " bg-yellow-600/50 border border-yellow-400/70"
+    const WhatUserLevel = " border-[0.2rem] font-extrabold"
 
     let levelStyle = []
 
     function leveler() {
         for (let i = 1; i < 100; i++) {
             if (i < 11) {
-                levelStyle.push(En_Ti)
+                if ($user.level >= i) {
+                    levelStyle.push(En_Ti + WhatUserLevel)
+                } else {
+                    levelStyle.push(En_Ti)
+                }
             }
             if (i > 10 && i < 26) {
-                levelStyle.push(Elve_Tjuefem)
+                if ($user.level >= i) {
+                    levelStyle.push(Elve_Tjuefem + WhatUserLevel)
+                } else {
+                    levelStyle.push(Elve_Tjuefem)
+                }
             }
             if (i > 25 && i < 56) {
-                levelStyle.push(Tjueseks_Femtifem)
+                if ($user.level >= i) {
+                    levelStyle.push(Tjueseks_Femtifem + WhatUserLevel)
+                } else {
+                    levelStyle.push(Tjueseks_Femtifem)
+                }
             }
             if (i > 55 && i < 81) {
-                levelStyle.push(Femtiseks_Åtti)
+                if ($user.level >= i) {
+                    levelStyle.push(Femtiseks_Åtti + WhatUserLevel)
+                } else {
+                    levelStyle.push(Femtiseks_Åtti)
+                }
             }
             if (i > 80 && i < 100) {
-                levelStyle.push(Åttien_Nittini)
+                if ($user.level >= i) {
+                    levelStyle.push(Åttien_Nittini + WhatUserLevel)
+                } else {
+                    levelStyle.push(Åttien_Nittini)
+                }
             }
             
         }
@@ -67,17 +88,56 @@
             <div class=" flex items-center justify-between">
                 <Avatar initials={$user.username} />
                 <p class=" font-extrabold text-2xl mx-6">{$user.username}</p>
-                <p class=" {userLevelStyle} w-7 h-7 flex justify-center items-center rounded-lg text-sm">{$user.level}</p>
+                <p class=" text-xs bg-surface-700 p-[0.15rem] px-2 rounded-lg "><span class=" text-surface-400 font-bold">email: </span>{$user.email}</p>
             </div>
             <button on:click={() => closeModal()}>
                 <img src="/expand.svg" alt="close profile">
             </button>
         </div>
-        <div class=" border border-surface-400 p-2  mt-4 rounded-lg">
+        <div class=" border-b border-surface-400 my-6"></div>
+        <div class=" pb-2">
+            <p class=" font-extrabold text-lg mb-2">Leveler</p>
             <div class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-3 overflow-x-auto px-4 pt-4 pb-4">
                 {#each levelStyle as style, i}
                     <div class="snap-start shrink-0 w-7 h-7 rounded-lg flex justify-center items-center text-sm {style}">{i+1}</div>
                 {/each}
+            </div>
+        </div>
+        <div class=" border-b border-surface-400 my-6"></div>
+        <div>
+            <p class=" font-extrabold text-lg mb-2">Spill statistikk</p>
+            <div class=" flex ">
+                <div class=" flex flex-1 p-2">
+                    <div class=" border border-primary-500/50 h-20 w-full rounded-lg bg-gradient-to-l from-primary-500/30 to-transparent flex items-center justify-between">
+                        <div class=" ml-3">
+                            <p class=" text-xs text-primary-500 font-medium">Favoritt spill</p>
+                            <p class=" font-extrabold">Blackjack</p>
+                        </div>
+                        <small class=" mr-3"></small>
+                    </div>
+                </div>
+                <div class=" flex flex-1 p-2 ">
+                    <div class=" border border-pink-400/50 h-20 w-full rounded-lg bg-gradient-to-l from-pink-400/30 to-transparent flex items-center justify-between">
+                        <div class=" ml-3">
+                            <p class=" text-xs text-pink-400 font-medium">Heldigste vinnet</p>
+                            <p class=" font-extrabold">Blackjack</p>
+                        </div>
+                        <div class=" mr-3 bg-pink-400/30 border border-pink-400 p-[0.35rem] px-1 rounded-lg  ">
+                            <p class=" text-xs font-extrabold">x11</p>
+                        </div>
+                    </div>
+                </div>
+                <div class=" flex flex-1 p-2">
+                    <div class=" border border-yellow-400/50 h-20 w-full rounded-lg bg-gradient-to-l from-yellow-400/30 to-transparent flex items-center justify-between">
+                        <div class=" ml-3">
+                            <p class=" text-xs text-yellow-400 font-medium">Største vinnet</p>
+                            <p class=" font-extrabold">Blackjack</p>
+                        </div>
+                        <div class=" mr-3 bg-yellow-600/30 border border-yellow-400/70 p-[0.35rem] px-1 rounded-lg">
+                            <p class=" text-xs font-extrabold">x11</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
